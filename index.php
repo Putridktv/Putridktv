e17aeccf363fb71c163873df33229c4cc111f832
yandiaan <diansetiawan2121@gmail.com>
2021-10-20 01:02:28 +0700

    q

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Dian Setiawan(205410048)</h3>"
+echo "<h3>Dian Setiawan(205410048)</h3>";
+echo "<h3>Dian Setiawan(205410048sadasd)</h3>"
 
 ?>
