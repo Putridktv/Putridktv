13b8e29b79e100dbdecc8d6e3c58dc6ac637bbae
Putridktv <putridwi490@gmail.com>
2021-10-20 01:20:56 +0700

    last commit

--- index.php
+++ index.php
@@ -2,7 +2,5 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3>Putri Dwi Oktaviani(205410014)</h3>";
-echo "<h3>Putri Dwi Oktaviani(205410014)</h3>";
-echo "<h3>Putri Dwi Oktaviani(205410014)</h3>";
 
 ?>
