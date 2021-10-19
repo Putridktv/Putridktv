e52d5a21d39ea4973ef9eddd23dd2be83194c929
yandiaan <diansetiawan2121@gmail.com>
2021-10-20 00:09:18 +0700

    1.1.2 : commit repo bebas

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
+echo "<h3>Ng. Made Arya Putra Wibawa(205410065)</h3>"
+
+?>
